94b5ee1bfe346fff7b3bad1162bcfe34c31b9f88
Jean-Frederic Durand <jean-frederic.durand@corp.ovh.com>
2017-08-16 15:41:00 -0400

    added ProjectNumber following modification I made to ng-at-internet module

--- client/components/at-internet/at-internet.config.js
+++ client/components/at-internet/at-internet.config.js
@@ -1,59 +1,63 @@
 angular.module("managerApp")
-.config(function (atInternetProvider, atInternetUiRouterPluginProvider, CONFIG) {
-    "use strict";
-    var trackingEnabled = CONFIG.env === "production";
+    .config((atInternetProvider, atInternetUiRouterPluginProvider, CONFIG) => {
+        "use strict";
+        const trackingEnabled = CONFIG.env === "production";
 
-    atInternetProvider.setEnabled(trackingEnabled);
-    atInternetProvider.setDebug(!trackingEnabled);
+        atInternetProvider.setEnabled(trackingEnabled);
+        atInternetProvider.setDebug(!trackingEnabled);
 
-    atInternetUiRouterPluginProvider.setTrackStateChange(true);
-    atInternetUiRouterPluginProvider.addStateNameFilter(function (routeName) {
-        return routeName ? routeName.replace(/\./g, "::") : "";
-    });
+        atInternetUiRouterPluginProvider.setTrackStateChange(true);
+        atInternetUiRouterPluginProvider.addStateNameFilter(routeName => routeName ? routeName.replace(/\./g, "::") : "");
 
-})
-.config(["$provide", function ($provide) {
-    "use strict";
+    })
+    .config(["$provide", function ($provide) {
+        "use strict";
 
-    $provide.decorator("atInternet", function ($delegate, $q, $cookies, User, TRACKING) {
-        var delegateTrackPage = $delegate.trackPage;
-        var isDefaultConfigurationSet = false;
-        var trackPageRequestArgumentStack = [];
+        $provide.decorator("atInternet", ($delegate, $q, $cookies, CloudProject, User, TRACKING) => {
+            const delegateTrackPage = $delegate.trackPage;
+            let isDefaultConfigurationSet = false;
+            let trackPageRequestArgumentStack = [];
+            let projectNumber = "";
 
-        //Decorate trackPage to stack requests until At-internet default configuration is set
-        $delegate.trackPage = function () {
-            if (isDefaultConfigurationSet) {
-                delegateTrackPage.apply($delegate, arguments);
-            } else {
-                trackPageRequestArgumentStack.push(arguments);
-            }
-        };
+            // Decorate trackPage to stack requests until At-internet default configuration is set
+            $delegate.trackPage = function () {
+                if (isDefaultConfigurationSet) {
+                    delegateTrackPage.apply($delegate, arguments);
+                } else {
+                    trackPageRequestArgumentStack.push(arguments);
+                }
+            };
 
-        User.Lexi().get().$promise.then(function (me) {
-            var settings = angular.copy(TRACKING.config);
+            CloudProject.Lexi().get().$promise.then(projects => {
+                projectNumber = `[CloudProjects-${projects.length}]`;
+            }).catch(err => $q.reject(err));
 
-            var referrerSite = $cookies.get("OrderCloud");
-            if (referrerSite) {
-                settings.referrerSite = referrerSite;
-            }
+            User.Lexi().get().$promise.then(me => {
+                const settings = angular.copy(TRACKING.config);
 
-            settings.countryCode = me.country;
-            settings.currencyCode =  me.currency && me.currency.code;
-            $delegate.setDefaults(settings);
+                const referrerSite = $cookies.get("OrderCloud");
+                if (referrerSite) {
+                    settings.referrerSite = referrerSite;
+                }
 
-            isDefaultConfigurationSet = true;
+                settings.countryCode = me.country;
+                settings.currencyCode = me.currency && me.currency.code;
+                settings.projectNumber = projectNumber;
+                $delegate.setDefaults(settings);
 
-            _.forEach(trackPageRequestArgumentStack, function (trackPageArguments) {
-                delegateTrackPage.apply($delegate, trackPageArguments);
+                isDefaultConfigurationSet = true;
+
+                _.forEach(trackPageRequestArgumentStack, trackPageArguments => {
+                    delegateTrackPage.apply($delegate, trackPageArguments);
+                });
+
+            }).catch(err => {
+                $delegate.trackPage = function () {};
+                trackPageRequestArgumentStack = [];
+                $delegate.setEnabled(false);
+                return $q.reject(err);
             });
 
-        }).catch(function (err) {
-            $delegate.trackPage = function () {};
-            trackPageRequestArgumentStack = [];
-            $delegate.setEnabled(false);
-            return $q.reject(err);
+            return $delegate;
         });
-
-        return $delegate;
-    });
-}])
+    }]);
--- client/components/at-internet/at-internet.constants.js
+++ client/components/at-internet/at-internet.constants.js
@@ -1,8 +1,8 @@
 "use strict";
 
 angular.module("managerApp")
-.constant("TRACKING", {
-    config: {
-        level2: "1"    // 1 is Cloud project ID in AT-Internet manager
-    }
-});
+    .constant("TRACKING", {
+        config: {
+            level2: "1" // 1 is Cloud project ID in AT-Internet manager
+        }
+    });
