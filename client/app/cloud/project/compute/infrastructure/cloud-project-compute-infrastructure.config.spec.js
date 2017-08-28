2a8aec132507602eeeb30d1c43379666859f5391
Jean-Frederic Durand <jean-frederic.durand@corp.ovh.com>
2017-08-28 17:08:01 -0400

    implements identified visitor following changes made in ng-at0internet

---
+++ client/app/cloud/project/compute/infrastructure/cloud-project-compute-infrastructure.config.spec.js
@@ -0,0 +1,35 @@
+// "use strict";
+
+// describe("Controller config: CloudProjectComputeInfrastructureCtrl", function () {
+
+//     var controller;
+//     var util;
+
+
+//     beforeEach(module("managerAppMock"));
+
+//     beforeEach(inject(function (controllerTestUtil, _atInternet_) {
+//         util = controllerTestUtil;
+//         spyOn(_atInternet_, "trackEvent");
+//         controller = util.createController("CloudProjectComputeInfrastructureCtrl");
+//     }));
+
+//     afterEach(inject(function () {
+//         util.verifyHttpBackend();
+//     }));
+
+//     describe("atInternet trackEvent on initInfra", function () {
+//         it("should call trackEvent", inject(function ($httpBackend, atInternet) {
+//             // controller.Cloud = {};
+//             // controller.Cloud.Project = function () {};
+//             // controller.Cloud.Project().Lexi = function () {};
+//             // controller.Cloud.Project().Lexi().query = function() {};
+//             controller.serviceName = "";
+//             controller.initInfra();
+//             $httpBackend.flush();
+
+//             expect(atInternet.trackEvent).toHaveBeenCalled();
+//         }));
+//     });
+    
+// });
